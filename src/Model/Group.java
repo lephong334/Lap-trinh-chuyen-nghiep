efc97ecc35d56af34dd090dfedf6f3538d33e511
lephong334 <lephong334@gmail.com>
2021-03-06 10:53:32 +0700

    add chat

--- src/Model/Group.java
+++ src/Model/Group.java
@@ -6,8 +6,10 @@ import java.util.Random;
 
 class Group {
 	private String id;
-	private String name;
-	private User owner;
+	private String groupName;
+	private User groupOwner;
+	private List<String> listOfMessages;
+	private List<Media> listOfMedias;
 	protected List<User> listOfUsers;
 
 	public Group(List<User> listOfUsers) {
@@ -22,12 +24,36 @@ class Group {
 		this.id = id;
 	}
 
-	public String getName() {
-		return name;
+	public String getGroupName() {
+		return groupName;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setGroupName(String groupName) {
+		this.groupName = groupName;
+	}
+
+	public User getGroupOwner() {
+		return groupOwner;
+	}
+
+	public void setGroupOwner(User groupOwner) {
+		this.groupOwner = groupOwner;
+	}
+
+	public List<String> getListOfMessages() {
+		return listOfMessages;
+	}
+
+	public void setListOfMessages(List<String> listOfMessages) {
+		this.listOfMessages = listOfMessages;
+	}
+
+	public List<Media> getListOfMedias() {
+		return listOfMedias;
+	}
+
+	public void setListOfMedias(List<Media> listOfMedias) {
+		this.listOfMedias = listOfMedias;
 	}
 
 	public List<User> getListOfUsers() {
